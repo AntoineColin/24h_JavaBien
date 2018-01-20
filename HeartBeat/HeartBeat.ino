ce4495803cd4c034508d2b83fcaa8de6fec2382c
Quentin <quentinbrossier.t7@gmail.com>
2018-01-20 13:42:11 +0100

    Blinky !

---
+++ HeartBeat/HeartBeat.ino
@@ -0,0 +1,24 @@
+#define LED_GREEN A2
+#define LED_RED A3
+#define LED_YELLOW A4
+
+void setup() {
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_GREEN, HIGH);
+  delay(500);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_RED, HIGH);
+  delay(500);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(500);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, HIGH);
+  delay(500);
+  digitalWrite(LED_RED, LOW);
+}
