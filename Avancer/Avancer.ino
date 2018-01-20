cdc5e819ab9fcca1b61e62ff8673606ae5ab7d25
Quentin <quentinbrossier.t7@gmail.com>
2018-01-20 18:00:58 +0100

    Déplacement du robot (suivi de ligne)

---
+++ Avancer/Avancer.ino
@@ -0,0 +1,22 @@
+#include <Servo.h> 
+
+Servo motor_LEFT;
+Servo motor_RIGHT;
+
+void setup() {
+    
+  motor_LEFT.attach(D6, 900, 2100);
+  motor_RIGHT.attach(D5, 900, 2100);
+
+}
+
+void loop() {
+  asservir();
+}
+
+
+void asservir() {
+  motor_LEFT.write(1500);
+  motor_RIGHT.write(1500);
+}
+
---
+++ SuiviLigne/SuiviLigne.ino
@@ -0,0 +1,53 @@
+#include <Servo.h> 
+
+/**
+ * 0 : blanc
+ * 1 : noir
+ */
+#define LineR D0
+#define LineL D1
+
+
+Servo motor_LEFT;
+Servo motor_RIGHT;
+
+int LineRVal;
+int LineLVal;
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  pinMode(LineR, INPUT);
+  pinMode(LineL, INPUT);
+
+  motor_LEFT.attach(D6, 900, 2100);
+  motor_RIGHT.attach(D5, 900, 2100);
+
+}
+
+void loop() {
+  delay(10);
+  LineLVal = digitalRead(LineL);
+  LineRVal = digitalRead(LineR);
+  
+  //Serial.print(LineLVal); Serial.print(" "); Serial.print(LineRVal);
+
+  if(LineLVal == 0 && LineRVal == 0) {//Avancer tout droit
+      motor_LEFT.write(1600);
+      motor_RIGHT.write(1400);
+      //Serial.println(" : Tout droit !!!");
+  } else if(LineLVal == 0 && LineRVal == 1) { //Tourner à droite
+      motor_LEFT.write(1600);
+      motor_RIGHT.write(1600);
+      //Serial.println(" : Droite");
+  } else if(LineLVal == 1 && LineRVal == 0) { //Tourner à gauche
+      motor_LEFT.write(1400);
+      motor_RIGHT.write(1400);
+      //Serial.println(" : Gauche");
+  } else if(LineLVal == 1 && LineRVal == 1) { //Attendre
+      motor_LEFT.write(1480);
+      motor_RIGHT.write(1480);
+      //Serial.println(" : Attente");
+  }
+}
