514f57422ef9971f8f0f234181f1f0fd7e75ee65
Colin antoine <antoine35.colin@gmail.com>
2018-01-21 06:28:20 +0100

    Il faut rendre à César

---
+++ Cesar/Cesar/Cesar.ino
@@ -0,0 +1,26 @@
+
+#define SerialPort Serial
+
+
+String cesar(char code[]){
+
+  char reponse[60];
+  int i = 0;
+  while(code[i]!='\0'){
+    
+    reponse[i] = (char) (((int)code[i]+8-65)%26+65);
+    i++;
+  }
+  return reponse;
+}
+
+void setup() {
+  SerialPort.begin(9600);
+  String blo = cesar("MFKLEPPPAAWLVWKBWMPSNAFVMFME");
+  SerialPort.println(blo);  
+}
+
+
+void loop() {
+  
+}
